9c1272ce9a4080692fbc8b83512b3868521fe833
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-02-07 00:10:27 +0600

    last commit

--- problem.js
+++ problem.js
@@ -1,75 +1,83 @@
 /* declare a function with  mind Game and return final values */
 
 // function mindGame(number) {
-//     let input = number ;
-//     let multiple = input * 3;
-//     let addition = multiple + 10;
-//     let division = addition / 2;
-//     let substation = division - 5;
-//     return substation ;
+//     if(typeof(number) === "number"){
+//         let input = number ;
+//         let multiple = input * 3;
+//         let addition = multiple + 10;
+//         let division = addition / 2;
+//         let substation = division - 5;
+//         return substation ;
+//     }else{
+//         return "is not a number"
+//     }
 // }
-// const result = mindGame(15);
+// const result = mindGame(5);
 // console.log(result)
-
 
 /* mind game function code complete*/
-
-
 /*declare a function withe check even or odd number of length  */
 
-
 // function evenOdd(str){
-//     let input = str.length;
-//     if(input % 2 === 0){
-//         return `even`
+//     if(typeof(str) === "string"){
+//         let input = str.length;
+//         if(input % 2 === 0){
+//             return `even`
+//         }else{
+//             return `odd`
+//         }
 //     }else{
-//         return `odd`
+//         return "Not a valid input"
 //     }
 // }
-// let result = evenOdd("str ing");
+// let result = evenOdd("Phero");
 // console.log(result)
-
 
 /* evenOdd function code complete */
-
 /* isLGSeven Function pick less then seven number or return input multiple number */
+
 // function isLGSeven(number){
-//     let input = number;
-//     let inputSubstation = input - 7;
-//     if(inputSubstation < 7){
-//         return inputSubstation
+//     if(typeof(number) === 'number'){
+//         let input = number;
+//         let inputSubstation = input - 7;
+//         if(inputSubstation < 7){
+//             return inputSubstation
+//         }else{
+//             return input * 2
+//         }
 //     }else{
-//         return input * 2
+//         return "is not a number"
 //     }
 // }
-/* isLGSeven function code is complete */
-// const result = isLGSeven(15);
+// const result = isLGSeven(6);
 // console.log(result)
 
- /* problem 3 not a completed */
-
+/* isLGSeven function code is complete */
  /* findingBadData function return negative data list */
-const number = 'str';
- function findingBadData (arr) {
-    if(Array.isArray(arr) == true){
-        let negativeNum = [];
-    for(let i = 0 ; i<arr.length; i++){
-        let number = arr[i];
-        if(number < 0){
-            negativeNum.push(number);
-        }
-    }
-    return negativeNum.length
-    }else{
-        return "is not a array"
-    }
-    
- }
-const result = findingBadData(number) ;
-console.log(result)
-/*complete  find array good data and bad data  */
 
+// const number = 'str';
+//  function findingBadData (arr) {
+//     if(Array.isArray(arr) == true){
+//         let negativeNum = [];
+//     for(let i = 0 ; i<arr.length; i++){
+//         let number = arr[i];
+//         if(number < 0){
+//             negativeNum.push(number);
+//         }
+//     }
+//     return negativeNum.length
+//     }else{
+//         return "is not a array"
+//     }
+    
+//  }
+// const result = findingBadData(number) ;
+// console.log(result)
+
+
+/*complete  find array good data and bad data  */
 /*gemsToDiamond function Convert your gems into diamond */
+
 // function gemsToDiamond(friend1, friend2 , friend3){
 //     const friend1Value = friend1 * 21;
 //     const friend2Value = friend2 * 32;
@@ -82,7 +90,7 @@ console.log(result)
 //         return totalDiamond
 //     }
 // }
-
 // const result =gemsToDiamond(1, 1, 1);
 // console.log(result)
+
 /* gemsToDiamond function code is complete */
